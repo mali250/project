4028621f361a7d6406ee7a21479509d2330c0b57
mali250 <alimuazzam2000@gmail.com>
2021-08-25 15:01:36 +0500

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import Header from './Components/header';
+
+import './App.css';
+import Login from './Components/form';
+
+function App() {
+  return (
+    <div>
+<Header />
+<Login />
+
+  <h1 className="a">HELLO</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/header.js
@@ -0,0 +1,14 @@
+
+
+function Header (){
+
+    return(
+        <div>
+    <h1>HELLO</h1>(
+
+    <button  id="b" onClick={(alert("HELLO WORLD"))}>CLICK ME</button>
+    </div>
+    );
+
+}
+export default Header;
